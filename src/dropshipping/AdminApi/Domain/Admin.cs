cc683566cebf57cd7a9da7b402da5347531ef9cd
samuelsilvabezerrajunior <samuelsilvabezerrajunior@gmail.com>
2025-05-17 09:38:50 -0300

    update

---
+++ src/dropshipping/AdminApi/Domain/Admin.cs
@@ -0,0 +1,20 @@
+﻿namespace AdminApi.Domain
+{
+    public class Admin
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public bool? TwoFactorEnabled { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public virtual ICollection<User> Notifications { get; set; } = new List<User>();
+        public string Token { get; internal set; }
+    }
+}
